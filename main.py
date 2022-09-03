f1b15e1221f27dd2c6557e872201074dd02bb240
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2022-09-03 14:20:25 +0200

    feat: Add pump event as message

--- main.py
+++ main.py
@@ -2,13 +2,38 @@ import os
 import logging
 import time
 import datetime
+import binascii
 
 logging.basicConfig(format='%(asctime)s %(levelname)s [%(name)s] %(message)s', level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-from read_minimed_next24 import Medtronic600SeriesDriver
+from read_minimed_next24 import Medtronic600SeriesDriver, HISTORY_DATA_TYPE
+from pump_history_parser import AlarmNotificationEvent, AlarmClearedEvent
 from homeassistant_uploader import HomeAssistantUploader
+
+
+def get_pump_events(mt):
+    events_to_send = [AlarmNotificationEvent]
+
+    start_date = datetime.datetime.now() - datetime.timedelta(minutes=10)
+    history_pages = mt.getPumpHistory(None, start_date, datetime.datetime.max,
+                                      HISTORY_DATA_TYPE.PUMP_DATA)
+    events = mt.processPumpHistory(history_pages, HISTORY_DATA_TYPE.PUMP_DATA)
+
+    events_found = {}
+    for event in events:
+        if type(event) in events_to_send:
+            events_found[get_pump_event_id(event)] = event
+        if type(event) == AlarmClearedEvent:
+            if get_pump_event_id(event) in events_found:
+                del events_found[get_pump_event_id(event)]
+
+    return events_found
+
+
+def get_pump_event_id(event):
+    return binascii.hexlify(event.eventData[0x0B:][0:2])
 
 
 def get_and_upload_data():
@@ -38,6 +63,16 @@ def get_and_upload_data():
                         try:
                             status = mt.getPumpStatus()
                             uploader.update_states(status)
+                            events = get_pump_events(mt)
+
+                            if not events:
+                                uploader.update_event("")  # Reset message
+                            else:
+                                for event in events:
+                                    logger.info(events[event])
+                                    uploader.update_event(str(events[event]))
+                                    time.sleep(1)  # time to process event on Homeassistant
+
                             connected = True
                         except Exception:
                             logger.error("Unexpected error in client downloadOperations", exc_info=True)
