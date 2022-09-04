728c22b185d62e01f274f168d27e1802c471ce37
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2022-09-04 20:32:36 +0200

    feat: removal of application code from uploader

--- homeassistant_uploader/uploader.py
+++ homeassistant_uploader/uploader.py
@@ -15,42 +15,41 @@ class HomeAssistantUploader:
 
         self._client = Client(self._api_url, token)
 
-    def update_states(self, medtronic_pump_status):
-        if self._is_data_valid(medtronic_pump_status):
-            self._update_state(entity_id="sensor.minimed_bgl", state=medtronic_pump_status.sensorBGL)
-            self._update_state(entity_id="sensor.minimed_trend", state=medtronic_pump_status.trendArrow)
-            self._update_state(entity_id="sensor.minimed_active_insulin", state=medtronic_pump_status.activeInsulin)
-            self._update_state(entity_id="sensor.minimed_current_basal_rate",
-                               state=medtronic_pump_status.currentBasalRate)
-            self._update_state(entity_id="sensor.minimed_temp_basal_rate", state=medtronic_pump_status.tempBasalRate)
-            self._update_state(entity_id="sensor.minimed_temp_basal_rate_percentage",
-                               state=medtronic_pump_status.tempBasalPercentage)
-            self._update_state(entity_id="sensor.minimed_pump_battery_level",
-                               state=medtronic_pump_status.batteryLevelPercentage)
-            self._update_state(entity_id="sensor.minimed_insulin_units_remaining",
-                               state=medtronic_pump_status.insulinUnitsRemaining)
-            self.update_timestamp(state=str(medtronic_pump_status.sensorBGLTimestamp.strftime("%H:%M:%S %d.%m.%Y")))
-        else:
-            self.update_status("Invalid data.")
-            self.update_timestamp(datetime.datetime.now().strftime("%H:%M:%S %d.%m.%Y"))
-
-    @staticmethod
-    def _is_data_valid(medtronic_pump_status) -> bool:
-        invalid_sensor_values = [0, 770]
-        return str(medtronic_pump_status.sensorBGLTimestamp.strftime(
-            "%d.%m.%Y")) != "01.01.1970" and medtronic_pump_status.sensorBGL not in invalid_sensor_values
-
     def _update_state(self, entity_id, state):
         try:
             self._client.set_state(entity_id=entity_id, state=str(state))
         except pydantic.error_wrappers.ValidationError:
             pass
 
+    def update_bgl(self, state):
+        self._update_state(entity_id="sensor.minimed_bgl", state=state)
+
+    def update_trend(self, state):
+        self._update_state(entity_id="sensor.minimed_trend", state=state)
+
+    def update_active_insulin(self, state):
+        self._update_state(entity_id="sensor.minimed_active_insulin", state=state)
+
+    def update_current_basal_rate(self, state):
+        self._update_state(entity_id="sensor.minimed_current_basal_rate", state=state)
+
+    def update_temp_basal_rate(self, state):
+        self._update_state(entity_id="sensor.minimed_temp_basal_rate", state=state)
+
+    def update_temp_basal_rate_percentage(self, state):
+        self._update_state(entity_id="sensor.minimed_temp_basal_rate_percentage", state=state)
+
+    def update_pump_battery_level(self, state):
+        self._update_state(entity_id="sensor.minimed_pump_battery_level", state=state)
+
+    def update_insulin_units_remaining(self, state):
+        self._update_state(entity_id="sensor.minimed_insulin_units_remaining", state=state)
+
     def update_status(self, state):
-        self._update_state(entity_id="sensor.minimed_status", state=str(state))
+        self._update_state(entity_id="sensor.minimed_status", state=state)
 
     def update_timestamp(self, state):
-        self._update_state(entity_id="sensor.minimed_update_timestamp", state=str(state))
+        self._update_state(entity_id="sensor.minimed_update_timestamp", state=state)
 
     def update_event(self, state):
-        self._update_state(entity_id="sensor.minimed_message", state=str(state))
+        self._update_state(entity_id="sensor.minimed_message", state=state)
