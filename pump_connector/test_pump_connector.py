9bc5b087218f1e9e830fda71a771eb82829ffb01
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2022-10-06 16:12:57 +0200

    test: Add of first unit test

--- pump_connector/test_pump_connector.py
+++ pump_connector/test_pump_connector.py
@@ -9,23 +9,30 @@ class MockConnector:
         return True
 
 
-@pytest.fixture
-def unit_under_test(mocker):
-    mocker.patch('pump_connector.helper.get_datetime_now',
-                 return_value=datetime.datetime(2022, 1, 1, 12, 00, 00, 0))
-    return PumpConnector(connector=MockConnector())
+class TestPumpConnector:
 
+    @staticmethod
+    def create_unit_under_test():
+        return PumpConnector(connector=MockConnector())
 
-def test_wait(unit_under_test, mocker):
-    mocker.patch('pump_connector.helper.get_datetime_now',
-                 return_value=[datetime.datetime(2022, 1, 1, 12, 00, 00, 0),
-                               datetime.datetime(2022, 1, 1, 12, 5, 00, 0),
-                               datetime.datetime(2022, 1, 1, 12, 10, 00, 0),
-                               datetime.datetime(2022, 1, 1, 12, 15, 00, 0),
-                               datetime.datetime(2022, 1, 1, 12, 20, 00, 0),
-                               datetime.datetime(2022, 1, 1, 12, 30, 00, 0)])
-    mock_sleep = mocker.patch('time.sleep')
+    def mock_dependencies(self, mocker):
+        # pylint: disable=attribute-defined-outside-init
+        self.mock_sleep = mocker.patch("pump_connector.pump_connector.time.sleep")
+        self.mock_get_datetime_now = mocker.patch("pump_connector.pump_connector.get_datetime_now")
+        # pylint: enable=attribute-defined-outside-init
 
-    #unit_under_test.wait()
+    def test_wait(self, mocker):
+        self.mock_dependencies(mocker)
 
-    assert mock_sleep.call_count == 4
+        self.mock_get_datetime_now.side_effect = [datetime.datetime(2022, 1, 1, 12, 00, 00, 0),
+                                                  datetime.datetime(2022, 1, 1, 12, 00, 00, 0),
+                                                  datetime.datetime(2022, 1, 1, 12, 00, 5, 0),
+                                                  datetime.datetime(2022, 1, 1, 12, 00, 7, 0),
+                                                  datetime.datetime(2022, 1, 1, 12, 5, 30, 0),
+                                                  datetime.datetime(2022, 1, 1, 12, 10, 00, 0)]
+
+        unit_under_test = self.create_unit_under_test()
+        unit_under_test.wait()
+
+        assert unit_under_test._connection_timestamp == datetime.datetime(2022, 1, 1, 12, 00, 00, 0)
+        assert self.mock_sleep.call_count == 2
