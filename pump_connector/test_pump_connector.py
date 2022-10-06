2738ae5618f886cabb41ca710354d56e7a06880a
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2022-10-06 16:24:11 +0200

    refactor: Use magicmock for HomeAssistantConnector

--- pump_connector/test_pump_connector.py
+++ pump_connector/test_pump_connector.py
@@ -1,4 +1,4 @@
-import pytest
+from unittest.mock import MagicMock
 import datetime
 
 from pump_connector import PumpConnector
@@ -11,19 +11,20 @@ class MockConnector:
 
 class TestPumpConnector:
 
-    @staticmethod
-    def create_unit_under_test():
-        return PumpConnector(connector=MockConnector())
+    def create_unit_under_test(self):
+        return PumpConnector(connector=self.mock_connector)
 
     def mock_dependencies(self, mocker):
         # pylint: disable=attribute-defined-outside-init
+        self.mock_connector = mocker.patch("pump_connector.pump_connector.HomeAssistantConnector")
         self.mock_sleep = mocker.patch("pump_connector.pump_connector.time.sleep")
         self.mock_get_datetime_now = mocker.patch("pump_connector.pump_connector.get_datetime_now")
         # pylint: enable=attribute-defined-outside-init
 
-    def test_wait(self, mocker):
+    def test_wait_standard_waiting_time(self, mocker):
         self.mock_dependencies(mocker)
 
+        self.mock_connector.switched_on.return_value = True
         self.mock_get_datetime_now.side_effect = [datetime.datetime(2022, 1, 1, 12, 00, 00, 0),  # init of class
                                                   datetime.datetime(2022, 1, 1, 12, 00, 00, 0),  # first call of wait
                                                   datetime.datetime(2022, 1, 1, 12, 00, 5, 0),
