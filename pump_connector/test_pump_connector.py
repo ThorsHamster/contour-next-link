0c7e5614f7c61bf1ebc2fb8d70a61733b6d4aa0e
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2022-10-08 13:40:03 +0200

    test: Add of test_wait_switch_is_off

--- pump_connector/test_pump_connector.py
+++ pump_connector/test_pump_connector.py
@@ -43,3 +43,33 @@ class TestPumpConnector:
         unit_under_test.wait()
 
         assert self.mock_sleep.call_count == (5*60+30)/self.waiting_time_in_seconds
+
+    def test_wait_switch_is_off(self, mocker):
+        self.mock_dependencies(mocker)
+
+        self.mock_connector.switched_on.side_effect = 200 * [False]
+        self.mock_get_datetime_now.side_effect = self._generate_datetimes(
+            start_datetime=datetime.datetime(2022, 1, 1, 12, 00, 00, 0),
+            end_datetime=datetime.datetime(2022, 1, 1, 12, 10, 00, 0),
+            stepsize=datetime.timedelta(seconds=5))
+
+        unit_under_test = self.create_unit_under_test()
+
+        unit_under_test.wait()
+
+        assert self.mock_sleep.call_count == (5*60)/self.waiting_time_in_seconds
+
+    def test_wait_switch_is_off_switches_on_while_waiting(self, mocker):
+        self.mock_dependencies(mocker)
+
+        self.mock_connector.switched_on.side_effect = 20 * [False] + [True]
+        self.mock_get_datetime_now.side_effect = self._generate_datetimes(
+            start_datetime=datetime.datetime(2022, 1, 1, 12, 00, 00, 0),
+            end_datetime=datetime.datetime(2022, 1, 1, 12, 10, 00, 0),
+            stepsize=datetime.timedelta(seconds=5))
+
+        unit_under_test = self.create_unit_under_test()
+
+        unit_under_test.wait()
+
+        assert self.mock_sleep.call_count == 20
