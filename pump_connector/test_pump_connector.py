e7e9e1af843b3aa5f4301ce281c3694cefefaff1
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2023-03-01 10:47:34 +0100

    test: Adds test for set change event

--- pump_connector/test_pump_connector.py
+++ pump_connector/test_pump_connector.py
@@ -1,7 +1,9 @@
+from unittest.mock import Mock
 import datetime
 
 from pump_connector import PumpConnector
 from pump_data import MedtronicDataStatus, MedtronicMeasurementData
+from pump_history_parser import InsulinDeliveryStoppedEvent, InsulinDeliveryRestartedEvent
 
 
 class TestPumpConnector:
@@ -16,9 +18,11 @@ class TestPumpConnector:
         self.mock_sleep = mocker.patch("pump_connector.pump_connector.time.sleep")
         self.mock_get_datetime_now = mocker.patch("pump_connector.pump_connector.get_datetime_now")
         self.mock_medtronic_driver = mocker.patch("pump_connector.pump_connector.Medtronic600SeriesDriver")
+        self.mock_InsulinDeliveryStoppedEvent = Mock(spec=InsulinDeliveryStoppedEvent)
+        self.mock_InsulinDeliveryRestartedEvent = Mock(spec=InsulinDeliveryRestartedEvent)
         # pylint: enable=attribute-defined-outside-init
 
-    def test_get_and_upload_data_happy_path(self, mocker):
+    def test_get_and_upload_data_happy_path_no_events(self, mocker):
         self.mock_dependencies(mocker)
 
         mock_return = MedtronicMeasurementData(
@@ -42,12 +46,43 @@ class TestPumpConnector:
         self.mock_connector.update_trend.assert_called_with(state=mock_return.trend)
         self.mock_connector.update_active_insulin.assert_called_with(state=mock_return.active_insulin)
         self.mock_connector.update_current_basal_rate.assert_called_with(state=mock_return.current_basal_rate)
-        self.mock_connector.update_temp_basal_rate_percentage.assert_called_with(state=mock_return.temporary_basal_percentage)
+        self.mock_connector.update_temp_basal_rate_percentage.assert_called_with(
+            state=mock_return.temporary_basal_percentage)
         self.mock_connector.update_pump_battery_level.assert_called_with(state=mock_return.battery_level)
         self.mock_connector.update_insulin_units_remaining.assert_called_with(state=mock_return.insulin_units_remaining)
         self.mock_connector.update_status.assert_called_with("Connected.")
         self.mock_connector.update_timestamp.assert_called_with(state="12:00:00 01.01.2022")
         self.mock_connector.update_event.assert_called_with("")
+
+    def test_get_and_upload_data_event_set_change(self, mocker):
+        self.mock_dependencies(mocker)
+
+        mock_return = MedtronicMeasurementData(
+            bgl_value=120,
+            trend="No arrows",
+            active_insulin=1.1,
+            current_basal_rate=0.123,
+            temporary_basal_percentage=75,
+            battery_level=75,
+            insulin_units_remaining=92,
+            status=MedtronicDataStatus.valid,
+            timestamp=datetime.datetime(2022, 1, 1, 12, 00, 00, 0)
+        )
+        self.mock_medtronic_driver.return_value.getPumpMeasurement.return_value = mock_return
+
+        self.mock_InsulinDeliveryStoppedEvent.suspendReasonText = "Set change suspend"
+        self.mock_InsulinDeliveryStoppedEvent.timestamp = datetime.datetime(2022, 1, 1, 12, 00, 00, 0)
+        self.mock_medtronic_driver.return_value.processPumpHistory.return_value = [
+            self.mock_InsulinDeliveryRestartedEvent,
+            self.mock_InsulinDeliveryStoppedEvent,
+            self.mock_InsulinDeliveryRestartedEvent
+        ]
+
+        unit_under_test = self.create_unit_under_test()
+
+        unit_under_test.get_and_upload_data()
+
+        self.mock_connector.update_latest_set_change.assert_called_with("Saturday")
 
     def _generate_datetimes(self, start_datetime: datetime.datetime, end_datetime: datetime.datetime,
                             stepsize: datetime.timedelta) -> list:
